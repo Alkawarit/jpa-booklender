5d3539c8bb798ef673d238819965956ca7aeadd5
Alkawarit <a.a.alkawarit@gmail.com>
2022-02-13 19:21:45 +0100

    enum user role

---
+++ src/main/java/se/lexicon/aladdin/jpabooklender/model/constants/UserRole.java
@@ -0,0 +1,11 @@
+package se.lexicon.aladdin.jpabooklender.model.constants;
+
+public enum UserRole {
+
+    ROLE_PATIENT_USER(),
+    ROLE_PREMISES_ADMIN(),
+    ROLE_SUPER_ADMIN();
+
+    UserRole() {
+    }
+}
